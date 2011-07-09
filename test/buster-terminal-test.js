cde868d0fbf23f9a4ea52b9dcc66fc69c18a1d5e
Christian Johansen <christian@gitorious.org>
2011-07-09 19:51:22 +0200

    Browser-compatibility for both library and test

--- test/buster-terminal-test.js
+++ test/buster-terminal-test.js
@@ -1,33 +1,39 @@
-var buster = require("buster");
-var assert = buster.assert;
-var a = require("../lib/buster-terminal");
+if (typeof require == "function") {
+    var buster = require("buster");
+    buster.terminal = require("../lib/buster-terminal");
+}
 
-buster.testCase("Terminal string align test", {
-    "max width": {
-        "should get width of array of strings": function () {
-            assert.equals(a.maxWidth(["a", "b", "hey", "there"]), 5);
+(function () {
+    var assert = buster.assert;
+    var t = buster.terminal;
+
+    buster.testCase("Terminal string align test", {
+        "max width": {
+            "should get width of array of strings": function () {
+                assert.equals(t.maxWidth(["a", "b", "hey", "there"]), 5);
+            },
+
+            "should get width of array of strings and numbers": function () {
+                assert.equals(t.maxWidth(["a", 666782, 2, "there"]), 6);
+            },
+
+            "should count width of undefined as 0": function () {
+                assert.equals(t.maxWidth([null, undefined, false, ""]), 5);
+            }
         },
 
-        "should get width of array of strings and numbers": function () {
-            assert.equals(a.maxWidth(["a", 666782, 2, "there"]), 6);
-        },
+        "alignment": {
+            "should left align text": function () {
+                assert.equals(t.alignLeft("Hey there", 13), "Hey there    ");
+            },
 
-        "should count width of undefined as 0": function () {
-            assert.equals(a.maxWidth([null, undefined, false, ""]), 5);
+            "should right align text": function () {
+                assert.equals(t.alignRight("Hey there", 13), "    Hey there");
+            },
+
+            "should not pad too long text": function () {
+                assert.equals(t.alignRight("Hey there", 4), "Hey there");
+            }
         }
-    },
-
-    "alignment": {
-        "should left align text": function () {
-            assert.equals(a.alignLeft("Hey there", 13), "Hey there    ");
-        },
-
-        "should right align text": function () {
-            assert.equals(a.alignRight("Hey there", 13), "    Hey there");
-        },
-
-        "should not pad too long text": function () {
-            assert.equals(a.alignRight("Hey there", 4), "Hey there");
-        }
-    }
-});
+    });
+}());
